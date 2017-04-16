fb81266e18cca384b108789b51f08547f4664954
da7oom1988 <da7oom_nasser@hotmail.com>
2017-04-16 22:32:17 +0300

    2nd commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require('express');
-var cors = require('cors');
 var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
 
 function dateChange(d) {
     var dateForm = {month:"long", day:"numeric" , year: "numeric"};
@@ -23,17 +22,9 @@ function dateChange(d) {
 }
 
 app.get('/:input',function(req,res){
-    //res.json({name: "da7oom", age: "I won't tell you"})
-   // res.end(req.params.d);
    var result = dateChange(req.params.input);
    res.json(result);
 });
-
-
-
-
-
-
 
 
 app.listen(3000);
