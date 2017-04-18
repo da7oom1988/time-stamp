45fe501e9cd470b9de271f54bf5d70930892401b
da7oom1988 <da7oom_nasser@hotmail.com>
2017-04-18 09:22:46 +0300

    manging port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 
 
@@ -30,5 +31,7 @@ app.get('/',function(req,res){
    res.end("Enter the date in the url");
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 console.log("working");
